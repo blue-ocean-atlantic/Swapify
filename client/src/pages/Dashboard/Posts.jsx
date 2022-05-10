bfd740a1c4f17f31423f98b023d8f60bcef83c79
Cynthia-ann Telfair <ctelfair20@gmail.com>
2022-05-10 14:29:30 -0500

    changes size of post buttons

--- client/src/pages/Dashboard/Posts.jsx
+++ client/src/pages/Dashboard/Posts.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Button } from '@mantine/core';
 
+import NewPostButton from './NewPostButton.jsx';
+import './Posts.scss';
+
 function Posts() {
   return (
     <div className="dash-all-posts">
       <div className="dash-post-tabs">
-        <Button color="orange" radius="md" size="md" onClick={() => console.log('hi from active')}>Active</Button>
-        <Button color="orange" radius="md" size="md" onClick={() => console.log('hi from Given')}>Given</Button>
-        <Button color="orange" radius="md" size="md" onClick={() => console.log('hi from Received')}>Received</Button>
+        <Button color="orange" radius="md" size="sx" onClick={() => console.log('hi from active')}>Active</Button>
+        <Button color="orange" radius="md" size="sx" onClick={() => console.log('hi from Given')}>Given</Button>
+        <Button color="orange" radius="md" size="sx" onClick={() => console.log('hi from Received')}>Received</Button>
       </div>
       <div className="dash-post-box">
+        <NewPostButton />
         <ul>
           <li>Post</li>
           <li>Post</li>
