6daa308d19b5d186724b5a0ac7ae11629d2a7792
Ricky Polim <ricpolim@gmail.com>
2022-05-10 11:35:23 -0700

    add initial schemas for backend database

---
+++ server/db/model/Appointments.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const appointmentSchema = new mongoose.Schema({
+  appointment_id: {
+    type: Number,
+    index: true,
+    required: [true, 'please provide appointment_id'],
+  },
+  donor: { //references user_id
+    type: Number,
+    required: true
+  },
+  receiver: { //references user_id
+    type: Number,
+    required: true
+  },
+  listing_id: {
+    type: Number,
+    required: true,
+  },
+  time: {
+    type: Date,
+    required: true
+  }
+});
+appointmentSchema.index({ appointment_id: 1 });
+
+module.exports = mongoose.model('appointments', appointmentSchema);
